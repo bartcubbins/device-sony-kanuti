8100c7c61bf8a0c0324efa8ea5a2cb86c85aceab
Alin Jerpelea <alin.jerpelea@sony.com>
2018-08-08 14:12:35 +0200

    remove the duplicate androidboot.console
    the androidboot.console is already defined in the common git
    and should be removed from platforms
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -44,7 +44,7 @@ BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 
 # Serial console
-#BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0x7af0000 androidboot.console=msm_serial_dm,0x7af0000
+#BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0x7af0000
 
 TARGET_RECOVERY_FSTAB ?= $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/fstab.kanuti
 
